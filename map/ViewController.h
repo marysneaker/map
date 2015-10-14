18d12b5dbaaba953c765474baefa539f4ac56fdd
sinoswr <sinoswr@JeffersontekiMacBook-Pro.local>
2015-10-14 14:27:26 +0800

    Initial Commit

---
+++ map/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  map
+//
+//  Created by sinoswr on 15/10/14.
+//  Copyright (c) 2015年 sinoswr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ map/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  map
+//
+//  Created by sinoswr on 15/10/14.
+//  Copyright (c) 2015年 sinoswr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
